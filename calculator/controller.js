f39ea9aa3ae719c8e08c3356b5e2ce2955f1f793
Johan Burke <jburke@milestechnologies.com>
2021-05-03 11:19:48 -0400

    finished writing template

--- calculator/controller.js
+++ calculator/controller.js
@@ -1,61 +1,20 @@
+// setup variables and get a calculator instance
+function CreateCalculator() {}
 
-
-/**
- * set the global vars to the values from the UI
- */
-function CreateCalculator() {
-    var firstNumber = +document.getElementById("inputNumberOne").value;
-    var secondNumber = +document.getElementById("inputNumberTwo").value;
-    var operator = getOperator();
-
-    return new Calculator(firstNumber, secondNumber, operator);
-}
-
-
-  //Main Entry Point for Operate Click
- function calculate(){
-    // update the global vars with inputs from the UI
-    var calculator = CreateCalculator();
-    calculator.operate();
-    if(!calculator.attemptedDivideByZero){
-        updateResultText(
-            `The result of ${calculator.getAction()} ${calculator.firstNumber} and ${calculator.secondNumber} is ${calculator.value}`
-        );
-    }else{
-        updateResultText(`Don't divide by zero please.`);
-    }
-
- }
+// perform a calculation when the operator button is clicked
+function calculate() {}
 
 /**
  * set the text in the result section of the UI
  * @param {*} value
  */
-function updateResultText(value) {
-    document.getElementById("result").innerText = value;
-}
+function updateResultText(value) {}
 
-// main entry point for clear click
-function clearValues() {
-    document.getElementById("inputNumberOne").value = "";
-    document.getElementById("inputNumberTwo").value = "";
-    updateResultText("");
-    document.getElementById("inputNumberOne").focus();
-}
-
+// should clear input text values and focus the first number input
+function clearValues() {}
 
 /**
  * get the selected operator from the UI
  * @returns Operators
  */
-function getOperator() {
-    var selectedValue;
-    var radioButtons = document.getElementsByName("operator");
-    radioButtons.forEach((item) => {
-        if (item.checked) {
-            selectedValue = item.value;
-        }
-    });
-    return selectedValue;
-}
-
+function getOperator() {}
